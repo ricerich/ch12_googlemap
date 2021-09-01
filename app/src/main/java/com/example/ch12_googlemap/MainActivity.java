49e7d18a2cbd820f71a5edf8b88583bf11cdb263
KJB <drv98@naver.com>
2021-09-01 15:08:46 +0900

    12장 구글맵

---
+++ app/src/main/java/com/example/ch12_googlemap/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.ch12_googlemap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in1 = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(in1);
+
+            }
+        });
+    }
+}
